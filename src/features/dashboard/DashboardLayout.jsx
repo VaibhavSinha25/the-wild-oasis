942bfa771375cd683c678f522230f55868bb0ad6
Vaibhav <vaibhavsinha0825@gmail.com>
2025-01-21 19:58:31 +0530

    Implementing dark and light mode

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,14 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Todays activity</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
