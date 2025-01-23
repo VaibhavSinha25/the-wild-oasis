48509c69268a5a89d90c0b61fe318bdfe54965ba
Vaibhav <vaibhavsinha0825@gmail.com>
2025-01-23 13:09:00 +0530

    Changes

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
 import CabinTable from "../features/cabins/CabinTable";
 import Button from "../ui/Button";
 import CreateCabinForm from "../features/cabins/CreateCabinForm";
 import AddCabin from "../features/cabins/AddCabin";
-import CabinTableOperations from "../features/cabins/CabinTableOperations";
 
 function Cabins() {
   return (
