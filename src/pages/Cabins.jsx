a148cd5de5572b6579d4b3304e337478a11710de
Vaibhav <vaibhavsinha0825@gmail.com>
2025-01-23 13:01:18 +0530

    bug fix

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -6,7 +6,7 @@ import CabinTable from "../features/cabins/CabinTable";
 import Button from "../ui/Button";
 import CreateCabinForm from "../features/cabins/CreateCabinForm";
 import AddCabin from "../features/cabins/AddCabin";
-import CabinTableOperations from "../features/cabins/cabinTableOperations";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 
 function Cabins() {
   return (
